9c67aa2f7ac3d7545d1f79d308e8391a8614f31e
ramitbasyal2 <basyalramit02@gmail.com>
2026-06-13 00:57:51 +0545

    first commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,8 +38,8 @@ export const userSignup = async (req, res) => {
  const token =await genToken(user._id);
  res.cookie("token", token,{
        httpOnly:true,
-       secure: false,
-       sameSite:"strict",
+       secure: true,
+       sameSite:"none",
        maxAge: 7 * 24 * 60 * 60 * 1000
  })
 
@@ -83,8 +83,8 @@ export const userLogin = async (req,res) => {
             const token =await genToken(user._id);
             res.cookie("token", token,{
                 httpOnly:true,
-                secure: false,
-                sameSite:"strict",
+                secure: true,
+                sameSite:"none",
                 maxAge: 7 * 24 * 60 * 60 * 1000
             })
 
@@ -104,8 +104,8 @@ export const logout =  (req,res) => {
      try {
        res.clearCookie("token",{
              httpOnly:true,
-                secure: false,
-                sameSite:"strict",
+                secure: true,
+                sameSite:"none",
         })
         return res.status(200).json({success:true, message:"Logour successful"})
      } catch (error) {
